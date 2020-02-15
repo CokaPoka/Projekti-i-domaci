d21d5bbccad67a3b4a421d4f85874041ce2b4b7e
Jelena Dimitrijevski <jelenakikovic@yahoo.com>
2020-02-15 19:29:45 +0100

    Changed file d3.1.js

--- Nedelja3/d3.1.js
+++ Nedelja3/d3.1.js
@@ -1,13 +1,13 @@
 function divisibleByFive(arr) {
-        numbers='';
-        for(let index=0; index<arr.length; index++){
-         let element=arr[index];
-         if(element%5==0){
-             numbers+=element+' ';
-         }
-     }
-    
-    return numbers;
+    arr2 = [];
+    for (let index = 0; index < arr.length; index++) {
+        let element = arr[index];
+        if (element % 5 == 0) {
+            arr2.push(arr[index]);
+        }
+    }
+
+    return arr2;
 }
-let niz=[1,4,5,8,9,10,15,17];
+let niz = [1, 4, 5, 8, 9, 10, 15, 17];
 console.log(`Elementi deljivi sa 5 su: ${divisibleByFive(niz)}`);
